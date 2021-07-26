848bc3853b8d30c7372447838c90eb0fcf4cb38b
alok <aloknerurkar@gmail.com>
2021-07-26 17:16:59 +0530

    fix: deepsource

--- modules/repo/fsrepo/export_test.go
+++ modules/repo/fsrepo/export_test.go
@@ -1,11 +1,15 @@
 package fsrepo
 
+import (
+	"github.com/plexsysio/go-msuite/modules/repo"
+)
+
 var Opener = opener
 
 func GetRefCnt() int {
 	return opener.refCnt
 }
 
-func GetActive() *fsRepo {
+func GetActive() repo.Repo {
 	return opener.active
 }
